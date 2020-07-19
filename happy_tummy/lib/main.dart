1f0626df37f69f51c659d7c12beec64feeb5593c
suyogguragain <royguragain@gmail.com>
2020-07-19 14:43:51 +0545

    Initialy creating project and changing app icon

---
+++ happy_tummy/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title:Text('Happy Tummy'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    )
+  );
+}
